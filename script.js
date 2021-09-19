53ff96ae0c064192277988fb82e7c3fa0c657872
Hernan Di Tano <hditano@gmail.com>
2021-09-19 19:55:30 -0300

    added a few stuff

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const myArray = [];
 const divLogo = document.querySelector('.coin-logo');
 const getButton = document.querySelector('.getPVU');
-const firstRow = document.querySelector('.first-row');
-
+const mainTable = document.querySelector('tr.main-table');
+const firstRow = document.querySelector('.main-table');
 
 fetch('https://api.coingecko.com/api/v3/coins/list', {
         'method': 'GET'
@@ -31,32 +31,56 @@ function checkPvu() {
 getButton.addEventListener('click', getPvuPrice);
 
 function getPvuPrice() {
-    const options = {'method': 'GET', headers: {'accept': 'application/json'}}
-
-        const id = 'plant-vs-undead-token'
-        const vs_currency = 'usd';
-
-        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vs_currency}&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`, options)
-        .then(res => res.json())
-        .then(data => {
-            renderData(data[0].image,data[0].name, data[0].current_price, data[0].price_change_24h, data[0].price_change_percentage_24h);
-        })
+    const options = {
+        'method': 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
     }
-    
+
+    const coinsID = ['bitcoin', 'zcoin', 'cardano', 'tether', 'solana'];
+
+    let requests = coinsID.map(id => fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`))
+
+    Promise.all(requests)
+        .then(responses => {
+            for(let response of responses) {
+                console.log(`${response.url}: ${response.status}`);
+            }
+            return responses;
+        })
+        .then(responses => Promise.all(responses.map(r => r.json())))
+        .then(id => {for(let i = 0; i < id.length; i++) {
+                        for(let j = 0; j < id[i].length; j++) {
+                            renderData(id[i][j].image, id[i][j].name, id[i][j].current_price, id[i][j].price_change_24h, id[i][j].price_change_percentage_24h )
+                        }
+        }})
+}
+
 
 function renderData(logo, name, price, price_change, price_change_24) {
+
+
+
     let price_change_green = ``;
-    if(price_change >= 0) {
-        price_change_green = `<td class="price-change price-change-green">${price_change.toFixed(2)} %</td>` 
+    let price_change_green_24h = ``;
+    if (price_change >= 0) {
+        price_change_green = `<td class="price-change price-change-green">${price_change.toFixed(2)} USD</td>`
+        price_change_green_24h = `<td class="price-change price-change-green">${price_change_24.toFixed(2)} %</td>`
     } else {
-        price_change_green = `<td class="price-change price-change-red">${price_change.toFixed(2)} %</td>`
+        price_change_green = `<td class="price-change price-change-red">${price_change.toFixed(2)} USD</td>`
+        price_change_green_24h = `<td class="price-change price-change-red">${price_change_24.toFixed(2)} %</td>`
     }
-    firstRow.innerHTML = ` <img src="${logo}" class="logo-img">
+
+    if(mainTable) {
+        firstRow.innerHTML = ``;
+    }
+    firstRow.innerHTML += ` 
+                            <tr class="first-row">
+                            <td><img src="${logo}" class="logo-img"></td>
                             <td class="coin-name">${name.replace("Token","")}</td>
                             <td>${price.toFixed(2)}</td>
                             ${price_change_green}
-                            <td class="price-change">${price_change_24.toFixed(2)} %</td>
-                            <tr>
-                            </tr>`
+                            ${price_change_green_24h}
+                            <tr>`
 }
-
